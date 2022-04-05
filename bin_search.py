37cb10354f7965dabe61eb36250fcab443092297
ProgRiver <barkas.ssr@mail.ru>
2022-04-05 10:32:33 +0300

    file bin search 2

--- bin_search.py
+++ bin_search.py
@@ -6,7 +6,7 @@ def binary_search(list_in: list, key: int):
     end = len(list_in) - 1
 
     while first <= end:
-        centr = (first+end) // 2
+        centr = (first + end) // 2
         if list_in[centr] == key:
             return f"Индекс искомого элемента: {centr}"
         elif list_in[centr] > key:
@@ -18,4 +18,4 @@ def binary_search(list_in: list, key: int):
 
 
 nums = [1, 2, 5, 6, 8, 10, 12, 15, 17, 20]
-print(binary_search(nums, 0))
+print(binary_search(nums, 5))
